640a4481edc79c39f9f08326c65652b4a1276f0c
KataevAS <38090158+KataevAS@users.noreply.github.com>
2020-06-03 22:32:30 +0300

    initial

---
+++ precache-manifest.e084b50e6179e4a14d842df1bee52e5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e00a5c28ea4119c002ab9ddd2a73e5a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ec53dcaad485854b59d",
+    "url": "/static/css/main.02f7ebcb.chunk.css"
+  },
+  {
+    "revision": "3a5300dad614b033c30c",
+    "url": "/static/js/2.6824ba9f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6824ba9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ec53dcaad485854b59d",
+    "url": "/static/js/main.8108e35a.chunk.js"
+  },
+  {
+    "revision": "51ac0df437c1c295d630",
+    "url": "/static/js/runtime-main.0aaab9aa.js"
+  }
+]);
